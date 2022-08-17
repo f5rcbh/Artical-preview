9f43db806d6091249a9978b2fd3e7d9c2c1c2d67
f5rcbh <f6807@hotmail.com>
2022-08-17 04:51:18 +0300

    finish everything and ready to push IT

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const hover = document.querySelector('.share')
 
 const active = document.querySelector('.active')
 
+const shareImg = document.querySelector('path')
+
+const imgBackGround = document.querySelector('.img_share')
 
 hover.addEventListener("click",()=>{
     if(active.style.display == 'none'){
         active.style.display = 'inline-flex'
-        console.log('first')
+        shareImg.style.fill = '#fff'
+        imgBackGround.style.backgroundColor ='#6d7f97'
         
     }else{
         active.style.display = 'none'
+        shareImg.style.fill = '#6E8098'
+        imgBackGround.style.backgroundColor = '#ecf2f8'
 
     }
 })
